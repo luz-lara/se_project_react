383a42eaace0e0b975e4eb12b61834fca620a2b7
luz-lara <136045689+luz-lara@users.noreply.github.com>
2025-05-15 16:16:01 +0000

    changed header link to relative path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import { fetchWeatherData } from "../api";
 import logo from "../images/Logo.svg";
 import avatar from "../images/avatar.svg";
-import ToogleSwitch from "./ToggleSwitch";
+import ToggleSwitch from "./ToggleSwitch";
 import { Link } from "react-router-dom";
 
 function Header(props) {
   return (
     <header className="header">
       <div className="header__logo-and-location-container">
-        <Link to="./">
+        <Link to="/">
           <img src={logo} alt="wtwr logo" className="header__logo" />
         </Link>
         <p className="header__date-and-location">
@@ -16,11 +16,11 @@ function Header(props) {
         </p>
       </div>
       <div className="header__user-container">
-        <ToogleSwitch />
+        <ToggleSwitch />
         <button className="header__add-clothes-bttn" onClick={props.openmodal}>
           + Add clothes
         </button>
-        <Link to="./profile" className="no-underline">
+        <Link to="/profile" className="no-underline">
           <p className="header__username">Terrence Tegene</p>
         </Link>
         <img src={avatar} alt="user avatar" className="header__avatar" />
